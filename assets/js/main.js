5ef1d841a313be7f24ad9aafaa58732a7e3ad097
h3110 <desenvolvimento15@dl.com.br>
2025-02-24 07:46:57 -0300

    vs01

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+ $(document).ready(function(){
+    $('.slider').slick({
+      slidesToShow: 3,  // Mostra 3 slides por vez (ajuste se necessário)
+      slidesToScroll: 1, // Avança 1 slide por vez
+      autoplay: true, 
+      autoplaySpeed: 500, 
+      dots: true, 
+      arrows: false,
+      infinite: true, // Garante o loop infinito
+      cssEase: 'linear',
+      centerMode: true, // Define se os slides ficam centralizados
+      variableWidth: true// Evita que os slides tenham larguras variáveis
+    });
+  });
+
+  $(document).ready(function(){
+    $('.slider-2').slick({
+      slidesToShow: 3,  // Mostra 3 slides por vez (ajuste se necessário)
+      slidesToScroll: 1, // Avança 1 slide por vez
+      dots: true, 
+      arrows: true,
+      infinite: true, // Garante o loop infinito
+      cssEase: 'linear',
+      leftMode: true, // Define se os slides ficam centralizados
+      variableWidth: true// Evita que os slides tenham larguras variáveis
+    });
+  });
